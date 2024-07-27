7b59512299fcdbd7a167a7ac7471f4dc959e7220
crystalixxx <midrugov@edu.hse.ru>
2024-07-27 20:42:09 +0300

    added main files

---
+++ config.py
@@ -0,0 +1,5 @@
+class Settings:
+    DOWNLOAD_URL = "https://cloud.mail.ru/public/L1xB/nvgHGYJz5"
+
+
+settings = Settings()
---
+++ main.py
@@ -0,0 +1,4 @@
+from utils.download import download_file
+
+if __name__ == '__main__':
+   download_file()
