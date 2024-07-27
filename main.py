2d7a7d821101195df0871913967fe6ade70007db
crystalixxx <midrugov@edu.hse.ru>
2024-07-27 21:40:20 +0300

    reworked download for any files && reworked main because of it

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-from utils.download import download_file
+from utils.download import DownloadFile
+from utils.parse_csv import Parser
+from config import settings
 
 if __name__ == '__main__':
-   download_file()
+    DownloadFile.download()
+    columns, rows = Parser.parse()
+
+    print(columns)
+    print(rows)
+
--- utils/download.py
+++ utils/download.py
@@ -5,48 +5,34 @@ import csv
 from config import settings
 
 
-def get_main_download_link(link):
-    response = requests.get(link)
-    page_content = response.text
-
-    re_pattern = r'dispatcher.*?weblink_get.*?url":"(.*?)"'
-    match = re.search(re_pattern, page_content)
-
-    if match:
-        url = match.group(1)
-        parts = link.split('/')[-2:]
-        url = f'{url}/{parts[0]}/{parts[1]}'
-        return url
-
-    return None
-
-
 class DownloadFile:
-    def __init__(self, url, file_name):
-        self.__url = url
-        self.__file_name = file_name
+    __url = settings.get_url()
+    __file_name = settings.get_file_path()
 
-    def download_csv(self):
-        data = requests.get(self.__url)
+    @staticmethod
+    def download_csv():
+        data = requests.get(DownloadFile.__url)
         decoded_data = data.content.decode('utf-8')
 
         reader = csv.reader(decoded_data.splitlines(), delimiter=';', dialect='unix')
         listed_reader = list(reader)
 
-        with open(self.__file_name, "w") as file:
+        with open(DownloadFile.__file_name, "w") as file:
             for row in listed_reader:
                 file.write(",".join(row) + '\n')
 
-    def get_correct_downloader(self):
-        extension = self.__file_name.split('.')[-1]
+    @staticmethod
+    def download_json():
+        pass
+
+    @staticmethod
+    def download():
+        extension = DownloadFile.__file_name.split('.')[-1]
 
         if extension == 'csv':
-            self.download_csv()
+            DownloadFile.download_csv()
+        elif extension == 'json':
+            DownloadFile.download_json()
         else:
             raise Exception("Sorry, but we can't to process this file type now")
-
-
-def download_file():
-    downloader = DownloadFile(get_main_download_link(settings.DOWNLOAD_URL), settings.get_file_path())
-    downloader.get_correct_downloader()
 
