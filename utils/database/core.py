b6fbd1689e76b97bbd7f4ef5ceb9658847387234
crystalixxx <midrugov@edu.hse.ru>
2024-07-28 20:50:31 +0300

    fixed init script & maked feature of inserting record to pgsql table

--- utils/database/core.py
+++ utils/database/core.py
@@ -1,9 +1,25 @@
+import psycopg2
+
+
 class DatabaseSender:
-    def __init__(self, table_name: str, header: list, raws: list, columns_info: dict):
-        self.__table_name = table_name
+    def __init__(self, record_data: tuple, header: list, raws: list, columns_info: dict):
+        self.__record_data = record_data
         self.__header = header
         self.__raws = raws
         self.__columns_info = columns_info
 
     def create_record(self):
-        print(f"Table with name {self.__table_name} created!")
+        try:
+            with psycopg2.connect(dbname="postgres", host="localhost", user="admin", password="admin", port="5432") as conn:
+                with conn.cursor() as cursor:
+                    query = f"""
+                        INSERT INTO source(url, resource, table_name, extension)
+                        VALUES (%s, %s, %s, %s)
+                    """
+
+                    cursor.execute(query, self.__record_data)
+                    conn.commit()
+
+                    print(f"Table with name {self.__record_data[2]} created")
+        except Exception as e:
+            print(e)
--- utils/database/pgsql/init.sql
+++ utils/database/pgsql/init.sql
@@ -1,6 +1,6 @@
 CREATE TABLE source (
     id SERIAL PRIMARY KEY,
-    email VARCHAR NOT NULL,
+    url VARCHAR NOT NULL,
     resource VARCHAR,
     table_name VARCHAR NOT NULL,
     extension VARCHAR NOT NULL
