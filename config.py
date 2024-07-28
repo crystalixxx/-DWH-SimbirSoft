c64b84871b895dcc468c425b2728490b6187e18e
crystalixxx <midrugov@edu.hse.ru>
2024-07-28 18:47:10 +0300

    made new sctucture of utils folder

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-from utils.url import UrlHandler
+from utils.download.url import UrlHandler
 
 
 class Settings:
--- main.py
+++ main.py
@@ -1,11 +1,8 @@
-from utils.download import DownloadFile
+from utils.download.download import DownloadFile
 from utils.parse import Parser
-from config import settings
+
 
 if __name__ == '__main__':
     DownloadFile.download()
     columns, rows = Parser.parse()
-
-    print(columns)
-    print(rows)
 
