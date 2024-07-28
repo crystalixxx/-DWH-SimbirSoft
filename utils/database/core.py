038d8ca290707d63d1af456f89a67b4daa95155f
crystalixxx <midrugov@edu.hse.ru>
2024-07-28 20:06:54 +0300

    created base for DatabaseSender class

---
+++ utils/database/core.py
@@ -0,0 +1,9 @@
+class DatabaseSender:
+    def __init__(self, table_name: str, header: list, raws: list, columns_info: dict):
+        self.__table_name = table_name
+        self.__header = header
+        self.__raws = raws
+        self.__columns_info = columns_info
+
+    def create_table(self):
+        print(f"Table with name {self.__table_name} created!")
